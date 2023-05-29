73f7aacff826509e2ff5656d884f93b07fcd00fb
Fransiskus Rian Wardana Putra <f.rianwardanaputra@gmail.com>
2023-05-29 11:44:19 +0700

    Add helper functions

--- scripts/script.js
+++ scripts/script.js
@@ -17,6 +17,24 @@ function getComputerChoice() {
     return available[getRandomNumber(0, available.length - 1)];
 }
 
+function winMessage(weapon) {
+    const rockBeatsScissors = "You Win! Rock beats Scissors";
+    const scissorsBeatsPaper = "You Win! Scissors beat Paper";
+    const paperBeatsRock = "You Win! Paper beats Rock";
+    if (weapon === rock) return rockBeatsScissors;
+    if (weapon === scissors) return scissorsBeatsPaper;
+    if (weapon === paper) return paperBeatsRock;
+}
+
+function loseMessage(weapon) {
+    const rockBeaten = "You Lose! Rock is beaten by Paper";
+    const scissorsBeaten = "You Lose! Scissors is beaten by Rock";
+    const paperBeaten = "You Lose! Paper is beaten by Scissors";
+    if (weapon === rock) return rockBeaten;
+    if (weapon === scissors) return scissorsBeaten;
+    if (weapon === paper) return paperBeaten;
+}
+
 /**
  * play a single round of rock paper scissors
  * @param {string} playerSelection
@@ -27,44 +45,47 @@ function playSingleRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
-    const rockBeatsScissors = "Rock beats Scissors";
-    const scissorsBeatsPaper = "Scissors beat Paper";
-    const paperBeatsRock = "Paper beats Rock";
-    const youWin = "You Win! ";
-    const youLose = "You Lose! ";
     if (playerSelection === rock) {
-        if (computerSelection === paper) return youLose + paperBeatsRock;
-        if (computerSelection === scissors) return youWin + rockBeatsScissors;
+        if (computerSelection === paper) return loseMessage(rock);
+        if (computerSelection === scissors) return winMessage(rock);
     }
     if (playerSelection === paper) {
-        if (computerSelection === rock) return youWin + paperBeatsRock;
-        if (computerSelection === scissors) return youLose + scissorsBeatsPaper;
+        if (computerSelection === rock) return winMessage(paper);
+        if (computerSelection === scissors) return loseMessage(paper);
     }
     if (playerSelection === scissors) {
-        if (computerSelection === paper) return youWin + scissorsBeatsPaper;
-        if (computerSelection === rock) return youLose + rockBeatsScissors;
+        if (computerSelection === paper) return winMessage(scissors);
+        if (computerSelection === rock) return loseMessage(scissors);
     }
     return "It's a draw!";
+}
+
+/**
+ * get player's weapon of choice
+ * @returns {string}
+ */
+function getUserWeapon() {
+    let valid = false;
+    let playerChoice = "";
+    while (!valid) {
+        playerChoice = prompt("Choose your weapon: Rock, Paper, or Scissors?");
+        if (!available.includes(playerChoice))
+            alert(
+                "Error! Player selection not in Rock, Paper, Scissors! You can't bring " +
+                    playerChoice +
+                    " here!"
+            );
+        else valid = true;
+    }
+    return playerChoice;
 }
 
 function game() {
     let score = 0;
     for (let i = 0; i < 5; i++) {
-        let valid = false;
-        let playerChoice;
-        while (!valid) {
-            playerChoice = prompt(
-                "Choose your weapon: Rock, Paper, or Scissors?"
-            );
-            if (!available.includes(playerChoice))
-                alert(
-                    "Error! Player selection not in Rock, Paper, Scissors! You can't bring " +
-                        playerChoice +
-                        " here!"
-                );
-            else valid = true;
-        }
+        let playerChoice = getUserWeapon();
         let computerChoice = getComputerChoice();
+        console.log("It is: " + playerChoice + " vs " + computerChoice);
         let result = playSingleRound(playerChoice, computerChoice);
         console.log(result);
     }
